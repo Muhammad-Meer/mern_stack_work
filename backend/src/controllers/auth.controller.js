2751454f16ab39548748e4420351c88495d0b895
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-05-22 11:12:41 +0500

    newcode

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const UserSchema = require('../models/user.model');
 const bcrypt = require('bcrypt');
-
+const jwt = require("jsonwebtoken");
 
 
 async function userRegisterController(req, res) {
@@ -16,7 +16,7 @@ async function userRegisterController(req, res) {
       })
     }
 
-    console.log("An error occurred while registering the user")
+    console.log("Register request received")
 
 
     const UserisExist = await UserSchema.findOne({ email: email });
@@ -45,8 +45,64 @@ async function userRegisterController(req, res) {
   }
 }
 
+async function userLoginController(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required"
+      });
+    }
+
+    const user = await UserSchema.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    return res.status(200).json({
+      message: "Login successful",
+      user: {
+        username: user.username,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  }
+}
+
+async function userLogoutController(req, res) {
+  try {
+
+    return res.status(200).json({
+      message: "Logout successful"
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  }
+}
 
 
-
-module.exports = { userRegisterController }
+module.exports = { userRegisterController, userLoginController, userLogoutController }
 
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const authRouter = express.Router();
-const { userRegisterController } = require('../controllers/auth.controller')
+const { userRegisterController, userLoginController, userLogoutController } = require('../controllers/auth.controller')
 
-
-
-
-
-
-
-
-
-
+//        AUTH ROUTES
 authRouter.post('/register', userRegisterController)
+authRouter.post('/login', userLoginController)
+authRouter.post('/logout', userLogoutController)
+
+
+
+
 
 
 
