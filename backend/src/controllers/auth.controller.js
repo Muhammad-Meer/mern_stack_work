7cbe4ff8549a44989ce7c101ad580663aa39f64e
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-05-22 10:50:35 +0500

    newcode

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -9,6 +9,7 @@ async function userRegisterController(req, res) {
     const { username, email, password } = req.body;
 
 
+
     if (!username || !email || !password) {
       return res.status(400).json({
         message: "All fields are required to register a user"
