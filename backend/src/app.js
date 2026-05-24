f1a789affc3b65648b7b6042f8c80c11f5ef8e75
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-05-24 12:00:26 +0500

    newcodeis

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,28 +21,3 @@ app.use('/api/Food',createFoodrouter );
 
 
 module.exports = app;
-
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const authRouter = require('./routes/auth.routes');
-const createFoodrouter = require('./routes/food.partner.routes');
-
-
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
-
-
-
-
-
-
-app.use('/api/auth', authRouter);
-app.use('/api/Food',createFoodrouter );
-
-
-
-
-module.exports = app;
-
---
+++ frontend/src/Routes/AppRoutes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import reactrouter from 'react-router-dom'
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <div>AppRoutes</div>
+    </Router>
+  )
+}
+
+export default AppRoutes
