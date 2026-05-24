ec9040680f471e8652b78a26d76c2d9033082828
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-05-24 20:00:47 +0500

    newcode

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth.routes');
 const createFoodrouter = require('./routes/food.partner.routes');
+const cors = require('cors');
 
 
 const app = express();
@@ -11,8 +12,10 @@ app.use(cookieParser());
 
 
 
-
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
 app.use('/api/auth', authRouter);
 app.use('/api/Food',createFoodrouter );
