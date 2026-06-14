3bb17a6a42f76f883f54e62b3686d977d69486e5
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-06-14 15:05:01 +0500

    papan

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import AppRouter from './router/AppRouter'
+// import AppRouter from './router/AppRouter'
 
 const App = () => {
   return (
     <>
-    <AppRouter/>
+    {/* <AppRouter/> */}
     </>
   )
 }
